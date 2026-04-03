5566b5f7189e5db8a0a0ede1bbad6a9c2201b8a1
Ilia Gutu <ilia.gutu@gmail.com>
2026-04-03 14:21:26 +0200

    Regenerate SPM package

--- Package.swift
+++ Package.swift
@@ -28,24 +28,24 @@ let package = Package(
         dependencies: ["matrixbrownfield"]
       ),
       .binaryTarget(
-        name: "MatrixSDK",
+        name: "matrixbrownfield",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/MatrixSDK.xcframework.zip",
-        checksum: "b93067bf5826b18231dba9bfab4e21179b5988fa8f4d98f0a9104c661d53dbe1",
+        checksum: "e621c06943cc222127742e2c37b176136766cb68f95e785b37eb18a1afbdf1fd",
       ),
       .binaryTarget(
         name: "Clarity",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/Clarity.xcframework.zip",
-        checksum: "6421ee4d89c07d200a59bb0b96f3d6e597c75c7c5a54176d1beee641fda3aa6c",
+        checksum: "b87ffa94cb3e11afe0c87294fd841a59376018698ed0a25654567f91c424dddb",
       ),
       .binaryTarget(
         name: "OpenSSL",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/OpenSSL.xcframework.zip",
-        checksum: "60c4f945469992071ba0fdbb2912ce56733a262f365364e9fca354bd61ff9542",
+        checksum: "48a3b7dbd620b538ccba95333d22c3cdfa1985922a5825baf7a810cc9e54de61",
       ),
       .binaryTarget(
         name: "hermesvm",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/hermesvm.xcframework.zip",
-        checksum: "fbf6389eeed9f1f952a92188d006e531ee792e5c6bb8df9583325a04fba1785a",
+        checksum: "0ac8167c7a8c2797f0b0c1d0d87ab51a907f4457b139894228f6fd711c350ec1",
       ),
     ]
 )
