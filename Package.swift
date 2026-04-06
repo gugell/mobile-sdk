1d09a1bb9936559a470e061fa8bae16e71e3f73b
Ilia Gutu <ilia.gutu@gmail.com>
2026-04-06 15:15:18 +0200

    Added support for custom matrix url

--- Package.swift
+++ Package.swift
@@ -26,22 +26,22 @@ let package = Package(
       .binaryTarget(
         name: "MatrixSDK",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/MatrixSDK.xcframework.zip",
-        checksum: "12ee71ef3b7d8bbe7fffbe961ea5039b88cfa21079fa9023b31fe279b7a1ad66",
+        checksum: "99214151d99a3bda8e47ed96f5d5a7d3e38fc3820577dd8e633ef83202c32bde",
       ),
       .binaryTarget(
         name: "Clarity",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/Clarity.xcframework.zip",
-        checksum: "e06a4a8b647e4b39821accef93c845acb18c62778f4cedaafb28b3a8509d1b26",
+        checksum: "c92da742f5c3948de6e5b50bde3c55d4b2e3702858a093cef82c334486609e68",
       ),
       .binaryTarget(
         name: "OpenSSL",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/OpenSSL.xcframework.zip",
-        checksum: "306d514dc7ef48bf6cb662efbe3a255c2480b26bc89b7b04198f68bae95144fe",
+        checksum: "0b00589242a60ed4eb25fc83169f0043a1f19d6cbe04cb709426dcb15bc297ac",
       ),
       .binaryTarget(
         name: "hermesvm",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/hermesvm.xcframework.zip",
-        checksum: "7c5e1b3871b8e5617845ef0f6728c5f276b7980cd7c474368c9db9262cd54d90",
+        checksum: "d904a8a08e1f0ad6dcd538297d0b07ea8df44cfb19d3478d07d1371c0c083ef8",
       ),
     ]
 )
