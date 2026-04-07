568d0ea6ceed17f38b0984bbd24f82188c3ecc7a
Ilia Gutu <ilia.gutu@gmail.com>
2026-04-07 17:45:10 +0200

    Change linking method

--- Package.swift
+++ Package.swift
@@ -6,8 +6,12 @@ let package = Package(
     platforms: [.iOS(.v15)],
     products: [
       .library(
-        name: "MobileSDK",
-        targets: ["MobileSDK", "Clarity", "OpenSSL", "hermesvm"],
+        name: "MatrixSDK",
+        targets: ["MatrixSDK", "matrixbrownfield", "Clarity", "OpenSSL", "hermesvm"],
+      ),
+      .library(
+        name: "matrixbrownfield",
+        targets: ["matrixbrownfield"],
       ),
       .library(
         name: "Clarity",
@@ -24,24 +28,29 @@ let package = Package(
     ],
     targets: [
       .binaryTarget(
-        name: "MobileSDK",
-        url: "https://sdks.s3.eu-north-1.amazonaws.com/MobileSDK.xcframework.zip",
-        checksum: "434d0495fb8274e43308d3c39c7c3095e3c5b7df75f8ef0fa841cad9428f1689",
+        name: "MatrixSDK",
+        url: "https://sdks.s3.eu-north-1.amazonaws.com/MatrixSDK.xcframework.zip",
+        checksum: "d81e97d92860d93fa35c25d68e42ccf05d2e790f6e1fdd04cd55eb3ea67d6c35",
+      ),
+      .binaryTarget(
+        name: "matrixbrownfield",
+        url: "https://sdks.s3.eu-north-1.amazonaws.com/matrixbrownfield.xcframework.zip",
+        checksum: "730c2b66e1df4c16a2d88978bb53d76810d695e7c6d55aa3700fb84fce62fae2",
       ),
       .binaryTarget(
         name: "Clarity",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/Clarity.xcframework.zip",
-        checksum: "6de9eed7377ee013c1c2c0151d86cb44fdcd883828ff66cc70a51e16a96e4f6d",
+        checksum: "9ffaf710949011cb599f5a9bd26711915b55d66e74c82c454a14d40010c182eb",
       ),
       .binaryTarget(
         name: "OpenSSL",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/OpenSSL.xcframework.zip",
-        checksum: "cb65077372d701dcd1bcdc7c928b035f59f587be26f8d4db2bb1a6630b4e18d6",
+        checksum: "90263a4ee00c74a8ebdafd837945077961a915cdc0ca36199a2d5fccb82052cc",
       ),
       .binaryTarget(
         name: "hermesvm",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/hermesvm.xcframework.zip",
-        checksum: "cc48a988dcb47172dcd4406b5f5eadc65c59702505dffa74acbf3b5fabf1cd8a",
+        checksum: "1ee5a0febd4f234e501a1fcfafd8b27f60490c1b4bd972466d49635d9bce5759",
       ),
     ]
 )
