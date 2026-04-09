c86acbdf5226eb960aa762eba31f9170aabf9220
Ilia Gutu <ilia.gutu@gmail.com>
2026-04-09 15:10:12 +0200

    Improve mobile SDK linking

--- Package.swift
+++ Package.swift
@@ -7,34 +7,29 @@ let package = Package(
     products: [
       .library(
         name: "MatrixSDK",
-        targets: ["MatrixSDK"],
+        targets: ["MatrixSDK", "Clarity", "OpenSSL", "hermesvm"],
       ),
     ],
     targets: [
-      .target(
-        name: "MatrixSDK",
-        dependencies: ["MatrixSDKInternal", "Clarity", "OpenSSL", "hermesvm"],
-        path: "Sources/MatrixSDK",
-      ),
       .binaryTarget(
-        name: "MatrixSDKInternal",
-        url: "https://sdks.s3.eu-north-1.amazonaws.com/MatrixSDKInternal.xcframework.zip",
-        checksum: "172570ef270c32db0e51760475143329fa5831eef1bbccfde88669051c59c9a9",
+        name: "MatrixSDK",
+        url: "https://sdks.s3.eu-north-1.amazonaws.com/MatrixSDK.xcframework.zip",
+        checksum: "ffe823273237ddf1b1025fd11e10fffab8e9aa11ec96abec12e855536d96c552",
       ),
       .binaryTarget(
         name: "Clarity",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/Clarity.xcframework.zip",
-        checksum: "03a9dfb7b72351efd330c4037173c6e1213bf360932dbcd769073e429604bb6e",
+        checksum: "1548ca60c4cbd945ace47a12b5113d69b750a850470bf5963debd51c44cb0e1a",
       ),
       .binaryTarget(
         name: "OpenSSL",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/OpenSSL.xcframework.zip",
-        checksum: "11906902934d0ad41f9bdb47b1f7a82ce63eaebd586134cbfe9d111109a52481",
+        checksum: "cb396cf195a125317fa870c6a3096a77111caf90afbc234128f228ea0715528e",
       ),
       .binaryTarget(
         name: "hermesvm",
         url: "https://sdks.s3.eu-north-1.amazonaws.com/hermesvm.xcframework.zip",
-        checksum: "761b92726b23f6f732d0001bfee94ddde0f0898ebeb201afe54ecad399515ba9",
+        checksum: "d0a4b7225422f99a63c5c322c71b6e1ab551ae1fb0f41ea6314851bbb0e6b3bd",
       ),
     ]
 )
